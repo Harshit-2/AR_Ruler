b5b186b431ce7ffbef61018e6ea3f4df058ee058
Harshit <harshit242003@hotmail.com>
2024-11-28 15:07:36 +0530

    complete AR_Ruler

---
+++ AR RulerTests/AR_RulerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AR_RulerTests.swift
+//  AR RulerTests
+//
+//  Created by Harshit ‎ on 11/26/24.
+//
+
+import Testing
+@testable import AR_Ruler
+
+struct AR_RulerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
